8a9f3c5f871c443723d3b6c61c26412b511c81d0
Andrew Oakley <oakleyandrewj@gmail.com>
2021-12-22 15:00:22 +0000

    extended client model and seeding

--- Models/client.cs
+++ Models/client.cs
@@ -3,6 +3,14 @@ namespace CaseManAPI.Models;
 public class Client
 {
     public int Id { get; set; } 
-    public string? Name { get; set; }
+    public string? FirstName { get; set; }
+    public string? SecondName { get; set; }
+    public string? Initial { get; set; }
+    public string? Salutation { get; set; }
+    public string? Title { get; set; }
+    public string? HomeTel { get; set; }
+    public string? Mobile { get; set; }
+    public string? Email { get; set; }
+    public string? PrefContact { get; set; }
     public bool IsClientIDChecked { get; set; }
 }
--- Services/ClientService.cs
+++ Services/ClientService.cs
@@ -10,8 +10,28 @@ public static class ClientService
     {
         Clients = new List<Client>
         {
-            new Client {Id = 1, Name = "Mr John Smith", IsClientIDChecked = false},
-            new Client {Id = 2, Name = "Ms Jane Doe", IsClientIDChecked = true}
+            new Client {Id = 1, 
+            FirstName = "John",
+            SecondName = "Smith",
+            Initial = "A",
+            Salutation = "Dear Mr Smith",
+            Title = "Mr",
+            HomeTel = "039284092834",
+            Mobile = "092384098234",
+            Email = "jsmith@test.com",
+            PrefContact = "email",
+            IsClientIDChecked = false},
+            new Client {Id = 2, 
+            FirstName = "Jane",
+            SecondName = "Doe",
+            Initial = "B",
+            Salutation = "Dear Ms Doe",
+            Title = "Ms",
+            HomeTel = "0432324423432",
+            Mobile = "05685687965",
+            Email = "jdoe@test.com",
+            PrefContact = "mobile",
+            IsClientIDChecked = true},
         };
     }
 
